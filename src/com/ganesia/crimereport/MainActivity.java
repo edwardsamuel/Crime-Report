fa973b444ba9209eb7ca75ba5fa5c2fb0635a811
Anasthasia Amelia <anasthasia.amelia@gmail.com>
2014-11-08 17:38:49 +0700

    add a heatmap with dummy data

--- src/com/ganesia/crimereport/MainActivity.java
+++ src/com/ganesia/crimereport/MainActivity.java
@@ -1,26 +1,117 @@
 package com.ganesia.crimereport;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class MainActivity extends FragmentActivity {
 	
 	private SlidingUpPanelLayout mLayout;
+	private GoogleMap mMap;
+	private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+    private static final int ALT_HEATMAP_RADIUS = 20;
+       
+	private static final LatLng CHICAGO = new LatLng(41.8337329,-87.7321555);
+	
+	/**
+     * Maps name of data set to data (list of LatLngs)
+     * Also maps to the URL of the data set for attribution
+     */
+    private HashMap<String, DataSet> mLists = new HashMap<String, DataSet>();
+    
+    /**
+     * Helper class - stores data sets and sources.
+     */
+    private class DataSet {
+        private ArrayList<LatLng> mDataset;
+        private String mUrl;
+
+        public DataSet(ArrayList<LatLng> dataSet, String url) {
+            this.mDataset = dataSet;
+            this.mUrl = url;
+        }
+
+        public ArrayList<LatLng> getData() {
+            return mDataset;
+        }
+
+        public String getUrl() {
+            return mUrl;
+        }
+    }
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment_map)).getMap();
+		// mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CHICAGO, 10));
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-25, 143), 4)); // dummy: Melbourne
+		
+		try {
+            mLists.put(getString(R.string.crime_locations), new DataSet(readItems(R.raw.crime_locations),
+                    getString(R.string.crime_locations_url))); // please change the datasets of latlng in /res/raw/crime_locations.json
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
+        }
+		
+		// Check if need to instantiate (avoid setData etc twice)
+        if (mProvider == null) {
+            mProvider = new HeatmapTileProvider.Builder().data(
+                    mLists.get(getString(R.string.crime_locations)).getData()).build();
+            mOverlay = ((MapFragment) getFragmentManager().findFragmentById(R.id.fragment_map)).getMap().addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+            mProvider.setRadius(ALT_HEATMAP_RADIUS);
+        } else {
+            mProvider.setData(mLists.get(getString(R.string.crime_locations)).getData());
+            mProvider.setRadius(ALT_HEATMAP_RADIUS);
+            mOverlay.clearTileCache();
+        }
+
 		TopCrimeFragment topCrimeFrag = new TopCrimeFragment();
 		
 		//consumeReportAPI();
 	}
 	
+	// Datasets from http://data.gov.au
+    private ArrayList<LatLng> readItems(int resource) throws JSONException {
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        InputStream inputStream = getResources().openRawResource(resource);
+        String json = new Scanner(inputStream).useDelimiter("\\A").next();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject object = array.getJSONObject(i);
+            double lat = object.getDouble("lat");
+            double lng = object.getDouble("lng");
+            list.add(new LatLng(lat, lng));
+        }
+        return list;
+    }
+    
 //	protected void consumeReportAPI() {
 //		// Consume ReportAPI
 //		// Use Adapter from Retrofit Library
